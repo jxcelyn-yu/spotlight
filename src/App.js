16a1e74e03e1f3804ee0c1f509a60053b3071727
jxcelyn-yu <jocelynyu@berkeley.edu>
2024-05-03 13:44:47 -0700

    added scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
@@ -7,6 +9,23 @@ import Footer from './components/Footer';
 import Artist from './components/pages/Artists';
 import CallbackHandler from './components/CallbackHandler';
 import ArtistPage from './components/pages/ArtistPage';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.classList.add('scroll-to-top');
+
+    const timeoutId = setTimeout(() => {
+      document.body.classList.remove('scroll-to-top');
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -22,7 +41,8 @@ function App() {
 
   return (
     <Router>
-      <Navbar onSearch={handleSearch} /> {/* Pass handleSearch as a prop */}
+      <Navbar/>
+       <ScrollToTop />
       <Routes>
         <Route path="/callback" element={<CallbackHandler />} />
         <Route path="/" element={<Home />} />
